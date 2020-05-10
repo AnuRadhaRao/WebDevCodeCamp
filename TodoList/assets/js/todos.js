7341e8eeba4abac612e2292c2ee0ade8a320c2b9
Your Name <you@example.com>
2020-05-10 16:02:29 -0400

    Creating to-dos

--- TodoList/assets/js/todos.js
+++ TodoList/assets/js/todos.js
@@ -1,12 +1,23 @@
 // Check off specific To-Dos by clicking
-$("li").click(function(){
+$("ul").on("click","li", function(){
 	$(this).toggleClass("completed");
 });
 
 //click on "X" to delete
-$("span").click(function(){
+$("ul").on("click","span", function(){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(){
+	//Enter key == 13 (key code)
+	if(event.which === 13){
+		//add new todo element
+		var todo_element = $(this).val();
+		$(this).val("");
+		//create a new entry into list
+		$("ul").append("<li><span>X</span>" +todo_element +"</li>");
+	}
 });
